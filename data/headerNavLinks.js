51d226f5f4dcf38a6063941b5d5c3baeeec19ce7
blkluv <info@blkluv.org>
2023-09-28 19:06:51 -0400

    header

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
  { href: '/blog', title: 'Blog' },
- { href: '/journal', title: 'Journal' },
+ { href: '/donate', title: 'Donate' },
  { href: 'https://billing.stripe.com/p/login/eVaaH68crfwxf3WbII', title: 'Manage Donations' },
 ]
 
